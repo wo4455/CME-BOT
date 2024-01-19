bc9e5473d1cd861dd0c0d62d0ab832f6be821b4f
Will Otwell <90997466+wo4455@users.noreply.github.com>
2024-01-19 11:32:05 -0600

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import { useState } from 'react'
+import axios from 'axios';
+
+const App = () => {
+  return (
+    <h1>Hello World</h1>
+  )
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json()); 
+
+const { OpenAI } = require('openai');
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
+});
+
+app.post('/chat', async (req, res) => {
+    const { prompt } = req.body;
+
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: prompt }],
+        model: 'gpt-3.5-turbo',
+    });
+
+    // const completion = await openai.createCompletion({
+    //     model: 'gpt-3.5-turbo-1106',
+    //     max_tokens: 512,
+    //     temperature: 0,
+    //     // moderations:true,
+    //     prompt: prompt,
+    // });
+
+    for await (const chunk of stream) {
+        res.send(chunk.choices[0]?.delta?.content || 'No Response.');
+      }
+});
+
+app.get('/res', async (req, res) => {
+    const stream = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        stream: true,
+      });
+      for await (const chunk of stream) {
+        process.stdout.write(chunk.choices[0]?.delta?.content || 'No Response.');
+      }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port} successfully.`)
+});
