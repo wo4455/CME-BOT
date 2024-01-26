9ab57308096f9c10afde6110665b937d2b77f756
Will Otwell <90997466+wo4455@users.noreply.github.com>
2024-01-26 11:42:16 -0600

    Make the API Work

--- server/index.js
+++ server/index.js
@@ -15,41 +15,18 @@ const openai = new OpenAI({
     apiKey: process.env.API_KEY,
 });
 
-// app.post('/test', async (req, res) => { // doesnt work 
-//     const fakePrompt = req.body.prompt;
-//     res.send("Fake Prompt: " + fakePrompt);
-// });
-
-app.post('/chat', async (req, res) => {
-    const { prompt } = req.body;
-
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: prompt }],
-        model: 'gpt-3.5-turbo',
-    });
-
-    // const completion = await openai.createCompletion({
-    //     model: 'gpt-3.5-turbo-1106',
-    //     max_tokens: 512,
-    //     temperature: 0,
-    //     // moderations:true,
-    //     prompt: prompt,
-    // });
-
-    for await (const chunk of stream) {
-        res.send(chunk.choices[0]?.delta?.content || 'No Response.');
-      }
-});
-
 app.get('/res', async (req, res) => {
     const stream = await openai.chat.completions.create({
         model: 'gpt-4',
-        messages: [{ role: 'user', content: 'Say this is a test' }],
+        messages: [{ role: 'user', content: 'What are the best countries to travel to?' }],
         stream: true,
       });
       for await (const chunk of stream) {
-        process.stdout.write(chunk.choices[0]?.delta?.content || 'No Response.');
+        if (!chunk.choices[0].delta.content) continue;
+        const response = chunk.choices[0].delta.content;
+        process.stdout.write(response || 'No Response.');
       }
+      res.end();
 });
 
 const port = process.env.PORT || 3000;
